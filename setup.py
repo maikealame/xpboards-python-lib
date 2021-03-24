062036608ca49c77ee09c815e5fabed985d2da6b
Douglas Castro <eloydouglas00@gmail.com>
2021-03-24 17:43:42 -0300

    core: Improve setup and create services class

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xpboards',
+    packages=find_packages(include['xpboards']),
+    version='0.1.0',
+    description='A python util for xpboards',
+    author='Douglas Eloy',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==6.2.2'],
+    license='MIT'
+)   
+
---
+++ xpboards/services.py
@@ -0,0 +1,25 @@
+class XPBoardsServices:
+    def __init__(self, username, password):
+        self.__username = username
+        self.__password = password
+
+    def __generate_token():
+        pass
+
+    def get_token():
+        pass
+
+    def list_datasets():
+        pass
+
+    def list_datasets_items():
+        pass
+
+    def create_dataset():
+        pass
+
+    def update_dataset():
+        pass
+
+    def clear_dataset():
+        pass
